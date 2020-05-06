4670b71dc7b7728b5436718e96018166393cdbf9
Catherina <catherine.pchelina@gmail.com>
2020-05-06 16:53:53 -0700

    Add routes for drivers, passengers and trips

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :drivers, only: [:index, :show, :new, :create, :edit, :update]
+
+  resources :passengers, only: [:index, :show, :new, :create, :edit, :update]
+
+  resources :trips, only: [:index, :show, :new, :create, :edit, :update]
 end
