447632c50520428aead8bf3700a29345444f0e7c
quinqu <quinruby0@gmail.com>
2020-05-07 19:45:34 -0700

    total cost in model for passenger trips

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,5 +2,16 @@ class Passenger < ApplicationRecord
   validates :name, presence: true 
   validates :phone_num, presence: true 
   has_many :trips
+
+
+  def total_passenger_cost(passenger)
+    total_passenger_cost = 0 
+    passenger = Passenger.find_by(id: passenger)
+
+    passenger.trips.each do |trip| 
+      total_passenger_cost += trip.cost
+    end 
+    return sprintf('%.2f', total_passenger_cost)
+  end
   
 end
