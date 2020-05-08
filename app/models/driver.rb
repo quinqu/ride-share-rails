00ecdd5fa72283fe18198bc937a896f484355eaf
Catherina <catherine.pchelina@gmail.com>
2020-05-07 18:30:07 -0700

    Add total_earnings and average_rating methods for Driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,32 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def total_earnings(driver_id)
+    list_of_trips = Driver.find_by(id: driver_id).trips
+
+    total_earnings = 0
+
+    list_of_trips.each do |trip|
+      total_earnings += trip.cost
+    end
+
+    return total_earnings
+  end
+
+  def average_rating(driver_id)
+    list_of_trips = Driver.find_by(id: driver_id).trips
+    trips_count = list_of_trips.count 
+
+    if trips_count == 0
+      return "This driver has no trips"
+    end
+
+    total_ratings = 0
+
+    list_of_trips.each do |trip|
+      total_ratings += trip.rating 
+    end
+
+    return total_ratings / trips_count
+  end
 end
