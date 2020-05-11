a92768f7018dfc1b47b950132c8fe2899073be71
Catherina <catherine.pchelina@gmail.com>
2020-05-11 15:28:38 -0700

    Trip model: relations tests

--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -1,16 +1,65 @@
 require "test_helper"
 
 describe Trip do
+  let(:new_trip) {
+    passenger = Passenger.create(name: "Nina", phone_num: "12345")
+    driver = Driver.create(name: "Anna", vin: "qwert123", available: true)
+    Trip.create(date: "today", rating: 0, cost: Trip.trip_cost, driver_id: driver.id, passenger_id: passenger.id)
+  }
+
   it "can be instantiated" do
-    # Your code here
+    expect(new_trip.valid?).must_equal true
   end
 
   it "will have the required fields" do
-    # Your code here
+    new_trip.save 
+    trip = Trip.first 
+    [:date, :rating, :cost, :driver_id, :passenger_id].each do |field|
+      expect(trip).must_respond_to field
+    end
   end
 
   describe "relationships" do
-    # Your tests go here
+    
+    it "can set the driver through 'driver'" do 
+      passenger = Passenger.create(name: "Nina", phone_num: "12345")
+      driver = Driver.create(name: "Anna", vin: "qwert123", available: true)
+      trip = Trip.new(date: "today", rating: 0, cost: Trip.trip_cost)
+
+      trip.driver = driver 
+
+      expect(trip.driver_id).must_equal driver.id
+    end
+
+    it "can set the driver through 'driver_id'" do 
+      passenger = Passenger.create(name: "Nina", phone_num: "12345")
+      driver = Driver.create(name: "Anna", vin: "qwert123", available: true)
+      trip = Trip.new(date: "today", rating: 0, cost: Trip.trip_cost)
+
+      trip.driver_id = driver.id
+
+      expect(trip.driver).must_equal driver 
+    end
+
+    it "can set the passenger through 'passenger'" do 
+      passenger = Passenger.create(name: "Nina", phone_num: "12345")
+      driver = Driver.create(name: "Anna", vin: "qwert123", available: true)
+      trip = Trip.new(date: "today", rating: 0, cost: Trip.trip_cost)
+
+      trip.passenger = passenger 
+
+      expect(trip.passenger_id).must_equal passenger.id
+    end
+
+    it "can set the passenger through 'passenger_id'" do 
+      passenger = Passenger.create(name: "Nina", phone_num: "12345")
+      driver = Driver.create(name: "Anna", vin: "qwert123", available: true)
+      trip = Trip.new(date: "today", rating: 0, cost: Trip.trip_cost)
+
+      trip.passenger_id = passenger.id
+
+      expect(trip.passenger).must_equal passenger 
+    end
   end
 
   describe "validations" do
