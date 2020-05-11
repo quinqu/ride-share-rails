c9d0fc81571bf4a243018f674f2ece76b2ceb97a
quinqu <quinruby0@gmail.com>
2020-05-11 12:27:30 -0700

    trip tests

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -1,48 +1,57 @@
 require "test_helper"
 
 describe TripsController do
+  let (:trip) {
+    Trip.create
+  }
 
   describe "index" do 
-    it "" do 
+    it "can get a list of all trips" do 
+
+      get trips_path 
+
+      must_respond_with :success 
 
     end 
 
 
   end 
   describe "show" do
-    it "" do 
+    # it "can get trip" do 
+    #   get trip_path(trip.id)
+    #   must_respond_with :success
+    # end 
 
-    end 
-    it "" do 
+    # it "will redirect for invalid trip" do 
+    #   get trip_path(-1)
+    #   must_respond_with :redirect
 
-    end 
+    # end 
   end
 
   describe "create" do
-   
-    it "" do 
+    it "can request new trip" do 
 
-    end 
-    it "" do 
+      # #post trips_path, params: Trip.create
+      # trip = Trip.new
+      # post trips_path, params: :trip
+     
+      # expect(Trip.count).must_equal 1
 
     end 
   end
 
   describe "edit" do
-    it "" do 
+    it "will get valid trip" do 
 
-    end 
-    it "" do 
 
     end 
   end
 
   describe "update" do
-    it "" do 
-
-    end 
-    it "" do 
+    it "will edit rating" do 
 
     end 
   end
+
 end
